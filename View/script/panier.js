e409d5f6d200d0aba4c6e1f337632f9f17c4c269
adem-fennani <adem.fennani@esprit.tn>
2024-05-03 09:39:25 +0100

    Amélioration DELETE pour Commande

--- View/script/panier.js
+++
@@ -1,6 +0,0 @@
-// Function to display success alert
-function displaySuccessAlert() {
-  // Display success alert
-  alert("Commande soumise avec succès!");
-  return true; // Allow form submission
-}
